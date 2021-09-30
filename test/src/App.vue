6ec61ed33d8a49a45228ae71fa5e411d9a796df9
iampdy102 <iampdy102@gmail.com>
2021-10-01 02:40:22 +0900

    first commit

---
+++ test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <tHeader />
+    <div id="content" class="content">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import tHeader from "./components/layout/tHeader.vue";
+export default {
+  name: "App",
+  components: {
+    tHeader,
+  },
+};
+</script>
+
+<style>
+</style>
